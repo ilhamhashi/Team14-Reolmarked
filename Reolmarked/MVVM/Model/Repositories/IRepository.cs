cc2cf5cd74ef091b26db94913a5962192f61edc4
Ilham Hashi <iaha71375@edu.ucl.dk>
2025-09-10 13:45:46 +0300

    Added repositories for RentalAgreement, Shelf and Shelf_RentalAgreement

--- Reolmarked/MVVM/Model/Repositories/IRepository.cs
+++ Reolmarked/MVVM/Model/Repositories/IRepository.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Reolmarked.MVVM.Model.Repositories
+﻿namespace Reolmarked.MVVM.Model.Repositories
 {
-    internal class IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
     }
 }
---
+++ Reolmarked/MVVM/Model/Repositories/Shelf_RentalAgreementRepository.cs
@@ -0,0 +1,39 @@
+﻿using Reolmarked.MVVM.Model.Classes;
+
+namespace Reolmarked.MVVM.Model.Repositories
+{
+    public class Shelf_RentalAgreementRepository : IRepository<Shelf_RentalAgreement>
+    {
+        private readonly string _connectionString;
+
+        public Shelf_RentalAgreementRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Shelf_RentalAgreement> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shelf_RentalAgreement GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Shelf_RentalAgreement entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Shelf_RentalAgreement entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
