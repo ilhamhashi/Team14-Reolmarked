c71ddeb1b662738ceb847f90d916683ad6678d9f
MarianUCL <maha71397@edu.ucl.dk>
2025-09-11 08:33:25 +0200

    Nye klasser og repository

---
+++ Reolmarked/MVVM/Model/Classes/Paymentmethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reolmarked.MVVM.Model.Classes
+{
+    public class PaymentMethod
+
+    {
+    public int PaymentMethodId { get; set; }
+    public string Name { get; set; }
+
+    public double Fee { get; set; }
+
+
+    public PaymentMethod(int paymentMethodId, string name, double fee)
+    {
+
+        PaymentMethodId = paymentMethodId;
+        Name = name;
+        Fee = fee;
+
+    }
+
+ 
+}
+}
