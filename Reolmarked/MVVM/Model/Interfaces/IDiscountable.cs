dd915b6171df740bb0f255c6882f8ce40884e0be
Ilham Hashi <iaha71375@edu.ucl.dk>
2025-09-20 12:21:56 +0300

    added new interfaces

---
+++ Reolmarked/MVVM/Model/Interfaces/IDiscountable.cs
@@ -0,0 +1,10 @@
+﻿namespace Reolmarked.MVVM.Model.Interfaces
+{
+    public interface IDiscountable : ISellable
+    {
+        double Discount { get; }
+        double DiscountPctg { get; }
+
+        void ReduceSellingPrice();
+    }
+}
---
+++ Reolmarked/MVVM/Model/Interfaces/IRentable.cs
@@ -0,0 +1,11 @@
+﻿namespace Reolmarked.MVVM.Model.Interfaces
+{
+    public interface IRentable : IDiscountable
+    {
+        DateTime StartDate { get; set; }
+        DateTime? EndDate { get; set; }
+        bool IsRentalActive { get; set; }
+        void BeginRental();
+        void EndRental();
+    }
+}
